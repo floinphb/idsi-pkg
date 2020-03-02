f38e97b2768d7b6a6757c45a04f247b6e3f134fd
TOURE FLORENTIN <florentin.toure1@gamail.com>
2020-03-02 23:36:01 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+import setuptools
+
+setuptools.setup(
+    name='idsi_pkg',
+    version='1.0.0',
+    author='florentin.toure1@gmail.com',
+    description='Nore package',
+    packages=setuptools.find_packages(),
+    install_requires=[]
+)
